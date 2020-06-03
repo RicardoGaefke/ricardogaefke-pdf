fa0e54ba5fa950f0db0422248c877d11f73cffa7
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-06-03 09:39:43 -0300

    Cleans Web.Site project

--- src/Web.Site/Startup.cs
+++ src/Web.Site/Startup.cs
@@ -1,15 +1,11 @@
-using System;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
